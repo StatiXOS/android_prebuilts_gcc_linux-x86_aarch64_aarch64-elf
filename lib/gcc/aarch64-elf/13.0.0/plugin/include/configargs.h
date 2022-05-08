5c11bd6fde0e3ed15bd5fa7fa5ecbdad503881d5
Vaisakh Murali <mvaisakh@statixos.com>
2022-05-08 18:53:44 +0000

    Import ARM64 GCC 20220509
    StatiX Specific GCC
    
    Using built-in specs.
    COLLECT_GCC=./bin/aarch64-elf-gcc
    COLLECT_LTO_WRAPPER=/home/vaisakh/gcc/gcc-arm64/bin/../libexec/gcc/aarch64-elf/13.0.0/lto-wrapper
    Target: aarch64-elf
    Configured with: ../gcc/configure --target=aarch64-elf CFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections -lpthread' CXXFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections -lpthread' --prefix=/home/vaisakh/gcc/gcc-build/../gcc-arm64 --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Eva GCC for StatiX' --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 13.0.0 20220508 (Bleeding Edge) (Eva GCC for StatiX)
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ lib/gcc/aarch64-elf/13.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=aarch64-elf CFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections -lpthread' CXXFLAGS='-flto -O3 -pipe -ffunction-sections -fdata-sections -lpthread' --prefix=/home/vaisakh/gcc/gcc-build/../gcc-arm64 --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Eva GCC for StatiX' --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
